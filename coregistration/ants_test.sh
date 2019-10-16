19f726e7481203c2e000123f2463a94976551dc2
Charlotte Newton <ccn30@compute-image.localdomain>
2019-10-16 15:55:59 +0100

    try new ANTS call for epi coreg

--- coregistration/ants_test.sh
+++ coregistration/ants_test.sh
@@ -38,6 +38,28 @@ module load fsl/5.0.10
 	## Run ANTS quick reg T1 to EPI - works
 	#!antsRegistrationSyNQuick.sh -d 3 -f ${imagedirpath}/meantopup_Run_1.nii -m ${T1path}/reorientn4mag0000_PSIR_skulled_std.nii -o ${coreg}/T12epi_ANTS_whole
 
+	## Run ANTS syn full with parameters from Nick Tustison on SourceForge help thread 7d71cb7d64
+	antsRegistration \
+		--dimensionality 3 \
+		--float 0 \
+		--output [T12EPI_fullANTS_,T12EPI_fullANTS__Warped.nii.gz] \
+		--interpolation Linear --winsorize-image-intensities [0.005,0.995] \
+		--use-histogram-matching 0 \
+		--initial-moving-transform [${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1] \
+		--transform Rigid[0.1] \
+		--metric MI[${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1,32,Regular,0.20] \
+		--convergence [1000x500x100x0,1e-6,10] \
+		--shrink-factors 8x4x2x1 \
+		--smoothing-sigmas 3x2x1x0vox \
+		--transform SyN[0.1,3,0] \
+		--restrict-deformation 1x1x0 \
+		--metric CC[${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1,4] \
+		--convergence [10,1e-6,10] \
+		--shrink-factors 1 \
+		--smoothing-sigmas 0vox  \
+		--verbose
+
+
 	## concatenate matrices and warp t2 masks to EPI space - t12t1 should be t12t2. WORKS with NN - now try multilabel (but these were wrong epi <> T1 matrix - correct should be T12epi)- now try correct matrix with syn deformations with multilabel - WORKS BEST
 #!	antsApplyTransforms -d 3 \
 #!				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
@@ -61,13 +83,14 @@ module load fsl/5.0.10
 #!				-t [t12t1_ANTS_0GenericAffine.mat,1] \
 #!				-v
 
-	## now try correct matrix without syndeformations with multilabel
-	antsApplyTransforms -d 3 \
-				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
-				-r ${imagedirpath}/meantopup_Run_1.nii \
-				-o t12epimasks_affine_multilabel.nii.gz  \
-				-n MultiLabel \
-				-t T12epi_ANTS_whole0GenericAffine.mat \
-				-t [t12t1_ANTS_0GenericAffine.mat,1] \
-				-v
+	## now try correct matrix without syn deformations with multilabel - overlays on area of dropout...oriiginal EC or is EC distorted up which SyN picks up on?
+#!	antsApplyTransforms -d 3 \
+#!				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
+#!				-r ${imagedirpath}/meantopup_Run_1.nii \
+#!				-o t12epimasks_affine_multilabel.nii.gz  \
+#!				-n MultiLabel \
+#!				-t T12epi_ANTS_whole0GenericAffine.mat \
+#!				-t [t12t1_ANTS_0GenericAffine.mat,1] \
+#!				-v
+
 

4f293ac938ef880a658457ce74ced1a817b45fcf
Charlotte Newton <ccn30@compute-image.localdomain>
2019-10-17 11:41:16 +0100

    trying Denoise T1 on ANTS T1 to T2 and EPI to T1, single subject

--- coregistration/ants_test.sh
+++ coregistration/ants_test.sh
@@ -23,44 +23,50 @@ module load fsl/5.0.10
 	cd $coregdir
 	pwd
 	
-	## run N4BiasFieldCorrection
+	## run N4BiasFieldCorrection (now present in ASHS scripts) - old N4 in 27734 mp2rage/manualtestfiles
 
 	#!N4BiasFieldCorrection -i ${T2} -o ${pathstem}/raw_data/images/${subjID}/Series_033_Highresolution_TSE_PAT2_100/N4rSeries_033_Highresolution_TSE_PAT2_100_c32.nii
-	N4rT2=${pathstem}/raw_data/images/${subjID}/Series_033_Highresolution_TSE_PAT2_100/N4rSeries_033_Highresolution_TSE_PAT2_100_c32.nii
+	N4rT2=${pathstem}/raw_data/images/${subjID}/Series_033_Highresolution_TSE_PAT2_100/N4reorientSeries_033_Highresolution_TSE_PAT2_100_c32.nii
 	#!echo "Ran N4BiasFieldCorrection"
 	
-	## INSERT BEST ANTS FOR STRUCTURAL
-	#!antsRegistrationSyNQuick.sh -d 3 -f ${N4rT2} -m ${T1path}/reorientn4mag0000_PSIR_skulled_std.nii -o ${coreg}/t12t1_ANTS_
 
-	## Run ANTS quick reg EPI to T1 - nearly worked, try skulstripped t1 - no
+	## COREGISTRATION BELOW:
+
+	## 1 ANTS coreg for T1 to T2 structural (t12t1 should be t12t2) - works well **INCLUDE**
+	#!antsRegistrationSyNQuick.sh -d 3 -f ${N4rT2} -m ${T1path}/reorientn4mag0000_PSIR_skulled_std.nii -o ${coreg}/t12t1_ANTS_
+	## 2 try with DenoiseImage T1
+	antsRegistrationSyNQuick.sh -d 3 -f ${N4rT2} -m ${T1path}/manualtestfiles/denoisern4_PSIR_skulled_std.nii -o ${coreg}/t12t2_ANTSdenoise_
+
+	## Run ANTS quick reg EPI to T1 - nearly worked, try skulstripped t1 - not working
 	#!antsRegistrationSyNQuick.sh -d 3 -f ${T1path}/reorientn4mag0000_PSIR_skulled_std_struc_brain.nii -m ${imagedirpath}/meantopup_Run_1.nii -o ${coreg}/epi2T1_ANTS_ss_
 	
-	## Run ANTS quick reg T1 to EPI - works
+	## Run ANTS quick reg T1 to EPI - works but not optimal
 	#!antsRegistrationSyNQuick.sh -d 3 -f ${imagedirpath}/meantopup_Run_1.nii -m ${T1path}/reorientn4mag0000_PSIR_skulled_std.nii -o ${coreg}/T12epi_ANTS_whole
 
-	## Run ANTS syn full with parameters from Nick Tustison on SourceForge help thread 7d71cb7d64
-	antsRegistration \
-		--dimensionality 3 \
-		--float 0 \
-		--output [T12EPI_fullANTS_,T12EPI_fullANTS_Warped.nii.gz,T12EPI_fullANTS_InverseWarped.nii.gz] \
-		--interpolation Linear --winsorize-image-intensities [0.005,0.995] \
-		--use-histogram-matching 0 \
-		--initial-moving-transform [${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1] \
-		--transform Rigid[0.1] \
-		--metric MI[${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1,32,Regular,0.20] \
-		--convergence [1000x500x100x0,1e-6,10] \
-		--shrink-factors 8x4x2x1 \
-		--smoothing-sigmas 3x2x1x0vox \
-		--transform SyN[0.1,3,0] \
-		--restrict-deformation 1x1x0 \
-		--metric CC[${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1,4] \
-		--convergence [10,1e-6,10] \
-		--shrink-factors 1 \
-		--smoothing-sigmas 0vox  \
-		--verbose
+	## Run ANTS syn T1 to epi reg full with parameters from Nick Tustison on SourceForge help thread 7d71cb7d64 - EXCELLENT PERFORMANCE **INCLUDE**
+#!	antsRegistration \
+#!		--dimensionality 3 \
+#!		--float 0 \
+#!		--output [T12EPI_fullANTS_,T12EPI_fullANTS_Warped.nii.gz,T12EPI_fullANTS_InverseWarped.nii.gz] \
+#!		--interpolation Linear --winsorize-image-intensities [0.005,0.995] \
+#!		--use-histogram-matching 0 \
+#!		--initial-moving-transform [${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1] \
+#!		--transform Rigid[0.1] \
+#!		--metric MI[${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1,32,Regular,0.20] \
+#!		--convergence [1000x500x100x0,1e-6,10] \
+#!		--shrink-factors 8x4x2x1 \
+#!		--smoothing-sigmas 3x2x1x0vox \
+#!		--transform SyN[0.1,3,0] \
+#!		--restrict-deformation 1x1x0 \
+#!		--metric CC[${imagedirpath}/meantopup_Run_1.nii,${T1path}/reorientn4mag0000_PSIR_skulled_std.nii,1,4] \
+#!		--convergence [10,1e-6,10] \
+#!		--shrink-factors 1 \
+#!		--smoothing-sigmas 0vox  \
+#!		--verbose
 
+	## APPLYTRANSFORMS BELOW:
 
-	## concatenate matrices and warp t2 masks to EPI space - t12t1 should be t12t2. WORKS with NN - now try multilabel (but these were wrong epi <> T1 matrix - correct should be T12epi)- now try correct matrix with syn deformations with multilabel - WORKS BEST
+	## 1 concatenate matrices and warp t2 masks to EPI space - t12t1 should be t12t2. WORKS with NN - now try multilabel (but these were wrong epi <> T1 matrix - correct should be T12epi)- now try correct matrix with syn deformations with multilabel - WORKS BEST
 #!	antsApplyTransforms -d 3 \
 #!				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
 #!				-r ${imagedirpath}/meantopup_Run_1.nii \
@@ -71,7 +77,8 @@ module load fsl/5.0.10
 #!				-t t12t1_ANTS_1InverseWarp.nii.gz \
 #!				-t [t12t1_ANTS_0GenericAffine.mat,1] \
 #!				-v
-	## now try correct matrix with syn deformations with nearest neighbour	- WORKS BUT FEWER VOXELS IN EC MASK
+
+	## 2 now try correct matrix with syn deformations with nearest neighbour	- WORKS BUT FEWER VOXELS IN EC MASK
 #!	antsApplyTransforms -d 3 \
 #!				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
 #!				-r ${imagedirpath}/meantopup_Run_1.nii \
@@ -83,7 +90,7 @@ module load fsl/5.0.10
 #!				-t [t12t1_ANTS_0GenericAffine.mat,1] \
 #!				-v
 
-	## now try correct matrix without syn deformations with multilabel - overlays on area of dropout...oriiginal EC or is EC distorted up which SyN picks up on?
+	## 3 now try correct matrix without syn deformations with multilabel - overlays on area of dropout...oriiginal EC or is EC distorted up which SyN picks up on? Need to compare to SyN transofrmations at group level
 #!	antsApplyTransforms -d 3 \
 #!				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
 #!				-r ${imagedirpath}/meantopup_Run_1.nii \
@@ -93,4 +100,14 @@ module load fsl/5.0.10
 #!				-t [t12t1_ANTS_0GenericAffine.mat,1] \
 #!				-v
 
-
+	## 4 now try #1 with new epi to T1 coreg output (does it change difference between affine only reg above?)
+	antsApplyTransforms -d 3 \
+				-i ${segdirpath}/final/${subj}_left_lfseg_corr_nogray.nii.gz \
+				-r ${imagedirpath}/meantopup_Run_1.nii \
+				-o t12epimasks_syn_multilabel.nii.gz \
+				-n MultiLabel \
+				-t T12EPI_fullANTS_1Warp.nii.gz \
+				-t T12EPI_fullANTS_0GenericAffine.mat \
+				-t t12t1_ANTS_1InverseWarp.nii.gz \
+				-t [t12t1_ANTS_0GenericAffine.mat,1] \
+				-v
--- coregistration/coregbashtthisscript.sh
+++ coregistration/coregbashtthisscript.sh
@@ -5,13 +5,14 @@ pathstem=/lustre/scratch/wbic-beta/ccn30/ENCRYPT/gridcellpilot
 scriptdir=${pathstem}/scripts/coregistration
 
 submit=${scriptdir}/coreg_submit.sh
-mysubjs=${pathstem}/mysubjs_deflist.txt
+#!mysubjs=${pathstem}/mysubjs_deflist.txt
+subjID=27734/20190902_U-ID46027
 
 cd slurmoutputs
 
-for subjID in `cat $mysubjs`
-do
+#!for subjID in `cat $mysubjs`
+#!do
 	echo "Coregistration of:	$subjID"
 	sbatch ${submit} ${scriptdir} ${pathstem} ${subjID}
-done
+#!done
 	

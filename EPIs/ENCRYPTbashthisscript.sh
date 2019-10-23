623c8ad2a9c7ca9f5de41b860fa6eadc7e88e1b2
Charlotte Newton <ccn30@compute-image.localdomain>
2019-10-23 16:02:16 +0100

    rerun EPI preprocessing

--- EPIs/ENCRYPTbashthisscript.sh
+++ EPIs/ENCRYPTbashthisscript.sh
@@ -8,8 +8,8 @@ subjs_def=${myscriptdir}/ENCRYPT_subjects_parameters.m
 
 #! declare -a steporder=("raw" "skullstrip" "realign" "topup" "cat12")
 #! subjects_to_process=($(seq 1 1 10))
-#subjects_to_process=($(seq 1 1 10))
-subjects_to_process=1
+subjects_to_process=($(seq 1 1 10))
+#!subjects_to_process=1
 
 prevstep=raw
 step=realign
@@ -60,27 +60,27 @@ if [ "$job_state" == "FAILED" ]
 fi
 done
 
-jobIDs=""
-prevstep=
-step=
-for this_subj in ${subjects_to_process[@]}
-do 
-this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
+#!jobIDs=""
+#!prevstep=
+#!step=
+#!for this_subj in ${subjects_to_process[@]}
+#!do 
+#!this_job_id=$(sbatch --parsable ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step})
 #! echo ${submit} ${prepare} ${func} ${subjs_def} ${this_subj} ${clusterid} ${prevstep} ${step} #! for debug this will list all the sbatch submissions, to test, copy one line and paste after "sbatch "
-jobIDs="$jobIDs $this_job_id"
-done
+#!jobIDs="$jobIDs $this_job_id"
+#!done
 
-echo "submitted job stage" ${step} "polling for output before moving on"
-${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
-for this_job in ${jobIDs[@]}
-do
-this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
-job_state=`echo $this_job_outcome | awk '{print $1}'` 
-if [ "$job_state" == "FAILED" ] 
-    then
-        echo "SLURM submission failed - jobs went into error state"
-        exit 1;
-fi
-done
+#!echo "submitted job stage" ${step} "polling for output before moving on"
+#!${myscriptdir}/waitForSlurmJobs.pl 1 120 $jobIDs
+#!for this_job in ${jobIDs[@]}
+#!do
+#!this_job_outcome=$(sacct -j ${this_job} --format='State' -n)
+#!job_state=`echo $this_job_outcome | awk '{print $1}'` 
+#!if [ "$job_state" == "FAILED" ] 
+#!    then
+#!        echo "SLURM submission failed - jobs went into error state"
+#!        exit 1;
+#!fi
+#!done
 
 cd ..

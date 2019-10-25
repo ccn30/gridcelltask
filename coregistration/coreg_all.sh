6931d1dda3718093334137f32e080c4d31d3c321
Charlotte Newton <ccn30@compute-image.localdomain>
2019-10-25 17:38:02 +0100

    trying ANTS and ITK affine mask transformation

--- coregistration/coreg_all.sh
+++ coregistration/coreg_all.sh
@@ -10,7 +10,7 @@
 	echo $subject
 	
 	# set subject-wise paths
-	imagedirpath=${pathstem}/preprocessed_data/images/${subject}
+	imagedirpath=${pathstem}/preprocessed_data/images/old_data/${subject}
 	T1path=${pathstem}/raw_data/images/${subjID}/mp2rage
 	N4T2=${pathstem}/raw_data/images/${subjID}/Series_033_Highresolution_TSE_PAT2_100/N4Series_033_Highresolution_TSE_PAT2_100_c32.nii
 	#!N4T2=${pathstem}/raw_data/images/${subjID}/Series_033_Highresolution_TSE_PAT2_100/N4reorientSeries_033_Highresolution_TSE_PAT2_100_c32.nii	
@@ -36,8 +36,8 @@
 	cd ${coregdir}
 	pwd
 	
-	echo "EXECUTING t2 to t1 coreg: " $subject	
-	$antsroot/antsRegistrationSyNQuick.sh -d 3 -f ${N4T2} -m ${T1path}/denoiseRn4mag0000_PSIR_skulled_std.nii -o ${coregdir}/T1toT2_ANTS_
+#!	echo "EXECUTING t2 to t1 coreg: " $subject	
+#!	$antsroot/antsRegistrationSyNQuick.sh -d 3 -f ${N4T2} -m ${T1path}/denoiseRn4mag0000_PSIR_skulled_std.nii -o ${coregdir}/T1toT2_ANTS_
 
 	#### ---------- COREGISTER EPIS TO T1 --------- ####
 	
@@ -46,36 +46,36 @@
 	#!do
 		
 		# run second time with skullstripped T1 instead of whole brain
-		echo "EXECUTING epi to T1 coregistration for subject ${subject} using:"
+#!		echo "EXECUTING epi to T1 coregistration for subject ${subject} using:"
 		epi=${imagedirpath}/topup_Run_1_split0000.nii
 		T1=${T1path}/denoiseRn4mag0000_PSIR_skulled_std_struc_brain.nii
 		echo "EPI: " $epi
 		echo "T1: " $T1
-		antsRegistration \
-			--dimensionality 3 \
-			--float 0 \
-			--output [T1toEPI_ssANTS_,T1toEPI_ssANTS_Warped.nii.gz,T1toEPI_ssANTS_InverseWarped.nii.gz] \
-			--interpolation Linear --winsorize-image-intensities [0.005,0.995] \
-			--use-histogram-matching 0 \
-			--initial-moving-transform [${epi},${T1},1] \
-			--transform Rigid[0.1] \
-			--metric MI[${epi},${T1},1,32,Regular,0.20] \
-			--convergence [1000x500x100x0,1e-6,10] \
-			--shrink-factors 8x4x2x1 \
-			--smoothing-sigmas 3x2x1x0vox \
-			--transform SyN[0.1,3,0] \
-			--restrict-deformation 1x1x0 \
-			--metric CC[${epi},${T1},1,4] \
-			--convergence [10,1e-6,10] \
-			--shrink-factors 1 \
-			--smoothing-sigmas 0vox  \
-			--verbose
+#!		antsRegistration \
+#!			--dimensionality 3 \
+#!			--float 0 \
+#!			--output [T1toEPI_ssANTS_,T1toEPI_ssANTS_Warped.nii.gz,T1toEPI_ssANTS_InverseWarped.nii.gz] \
+#!			--interpolation Linear --winsorize-image-intensities [0.005,0.995] \
+#!			--use-histogram-matching 0 \
+#!			--initial-moving-transform [${epi},${T1},1] \
+#!			--transform Rigid[0.1] \
+#!			--metric MI[${epi},${T1},1,32,Regular,0.20] \
+#!			--convergence [1000x500x100x0,1e-6,10] \
+#!			--shrink-factors 8x4x2x1 \
+#!			--smoothing-sigmas 3x2x1x0vox \
+#!			--transform SyN[0.1,3,0] \
+#!			--restrict-deformation 1x1x0 \
+#!			--metric CC[${epi},${T1},1,4] \
+#!			--convergence [10,1e-6,10] \
+#!			--shrink-factors 1 \
+#!			--smoothing-sigmas 0vox  \
+#!			--verbose
 		
-		if [ $? -eq 0 ]; then
-    			echo ">> EPI_REG OK: subject ${subject}/run ${this_run}"
-		else
-    			echo ">> EPI_REG FAIL: subject ${subject}/run ${this_run}"
-		fi
+#!		if [ $? -eq 0 ]; then
+#!    			echo ">> EPI_REG OK: subject ${subject}/run ${this_run}"
+#!		else
+#!    			echo ">> EPI_REG FAIL: subject ${subject}/run ${this_run}"
+#!		fi
 		
 
 	#### ---------- COREGISTER ASHS OUTPUT TO EPIS AND MAKE EC MASKS --------- ####
@@ -87,55 +87,52 @@
 			mkdir ${maskregdir}
 			echo "Making EPI Mask directory"
 		fi
-		#!echo "EXECUTING T2 mask to EPI space transformation for subject ${subject}"
+		echo "EXECUTING T2 mask to EPI space transformation for subject ${subject}"
 		
 		# use same EPI as for T1 to EPI coregistration
-
-#####****************** REWRITE to include ATNS affine t2-t1 only and either epi_reg converted, or ITK snap**********************###
 		
 		#----- DIFFEOMORPHIC SYN -----#
 		#left MTL
-		#!antsApplyTransforms -d 3 \
-				-i ${segdirpath}/ASHS_corinputs/final/${subject}_3_left_lfseg_corr_nogray.nii.gz \
-				-r ${epi} \
-				-o ${maskregdir}/LeftMTLmaskWarped_SyN.nii.gz \
-				-n MultiLabel \
-				-t T1toEPI_ANTS_1Warp.nii.gz \
-				-t T1toEPI_ANTS_0GenericAffine.mat \
-				-t T1toT2_ANTS_1InverseWarp.nii.gz \
-				-t [T1toT2_ANTS_0GenericAffine.mat,1] \
-				-v
+#!		antsApplyTransforms -d 3 \
+#!				-i ${segdirpath}/ASHS_corinputs/final/${subject}_3_left_lfseg_corr_nogray.nii.gz \
+#!				-r ${epi} \
+#!				-o ${maskregdir}/LeftMTLmaskWarped_SyN.nii.gz \
+#!				-n MultiLabel \
+#!				-t T1toEPI_ANTS_1Warp.nii.gz \
+#!				-t T1toEPI_ANTS_0GenericAffine.mat \
+#!				-t T1toT2_ANTS_1InverseWarp.nii.gz \
+#!				-t [T1toT2_ANTS_0GenericAffine.mat,1] \
+#!				-v
 
 		#right MTL
-		#!antsApplyTransforms -d 3 \
-				-i ${segdirpath}/ASHS_corinputs/final/${subject}_3_right_lfseg_corr_nogray.nii.gz \
-				-r ${epi} \
-				-o ${maskregdir}/RightMTLmaskWarped_SyN.nii.gz \
-				-n MultiLabel \
-				-t T1toEPI_ANTS_1Warp.nii.gz \
-				-t T1toEPI_ANTS_0GenericAffine.mat \
-				-t T1toT2_ANTS_1InverseWarp.nii.gz \
-				-t [T1toT2_ANTS_0GenericAffine.mat,1] \
-				-v
+#!		antsApplyTransforms -d 3 \
+#!				-i ${segdirpath}/ASHS_corinputs/final/${subject}_3_right_lfseg_corr_nogray.nii.gz \
+#!				-r ${epi} \				-o ${maskregdir}/RightMTLmaskWarped_SyN.nii.gz \
+#!				-n MultiLabel \
+#!				-t T1toEPI_ANTS_1Warp.nii.gz \
+#!				-t T1toEPI_ANTS_0GenericAffine.mat \
+#!				-t T1toT2_ANTS_1InverseWarp.nii.gz \
+#!				-t [T1toT2_ANTS_0GenericAffine.mat,1] \
+#!				-v
 	
-		#----- AFFINE -----#
+		#----- AFFINE ITK and ANTS -----#
 		#left MTL
-		#!antsApplyTransforms -d 3 \
+		antsApplyTransforms -d 3 \
 				-i ${segdirpath}/ASHS_corinputs/final/${subject}_3_left_lfseg_corr_nogray.nii.gz \
 				-r ${epi} \
-				-o ${maskregdir}/LeftMTLmaskWarped_affine.nii.gz \
+				-o ${maskregdir}/LeftMTLmaskWarped_ITKaffine.nii.gz \
 				-n MultiLabel \
-				-t T1toEPI_ANTS_0GenericAffine.mat \
+				-t [EPItoT1_ITK_affine_crosscorr.txt,1] \
 				-t [T1toT2_ANTS_0GenericAffine.mat,1] \
 				-v
 
 		#right MTL
-		#!antsApplyTransforms -d 3 \
+		antsApplyTransforms -d 3 \
 				-i ${segdirpath}/ASHS_corinputs/final/${subject}_3_right_lfseg_corr_nogray.nii.gz \
 				-r ${epi} \
-				-o ${maskregdir}/RightMTLmaskWarped_affine.nii.gz \
+				-o ${maskregdir}/RightMTLmaskWarped_ITKaffine.nii.gz \
 				-n MultiLabel \
-				-t T1toEPI_ANTS_0GenericAffine.mat \
+				-t [EPItoT1_ITK_affine_crosscorr.txt,1] \
 				-t [T1toT2_ANTS_0GenericAffine.mat,1] \
 				-v
 		
--- coregistration/coregbashtthisscript.sh
+++ coregistration/coregbashtthisscript.sh
@@ -6,15 +6,15 @@ scriptdir=${pathstem}/scripts/coregistration
 
 submit=${scriptdir}/coreg_submit.sh
 #!submit2=${scriptdir}/coreg_2_submit.sh
-#!mysubjs=${pathstem}/master_subjsdeflist.txt
-subjID=29336/20190903_U-ID46066
+mysubjs=${pathstem}/master_subjsdeflist.txt
+#!subjID=29336/20190903_U-ID46066
 #!mysubjs=${pathstem}/mysubjs_deflist.txt
 
 cd slurmoutputs
 
-#!for subjID in `cat $mysubjs`
-#!do
+for subjID in `cat $mysubjs`
+do
 	echo "ANTS coregistration of:	$subjID"
 	sbatch ${submit} ${scriptdir} ${pathstem} ${subjID}
 	#!sbatch ${submit2} ${scriptdir} ${pathstem} ${subjID}
-#!done	
+done	
